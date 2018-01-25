6dbfe4c11a291511d8e2562fe7cde7795b5a7692
kxekxe <shkipc@gmail.com>
2018-01-25 09:52:09 +0100

    add send_photo from camera

--- bot.py
+++ bot.py
@@ -7,11 +7,20 @@ from time import sleep
 from pyA20.gpio import connector
 import config
 import time
+import os
+
+def take_picture():
+    fn = '/root/doorbellbot/010.jpeg'
+    res = ''
+    os.system('cd /root/doorbellbot && avconv -t 1 -f video4linux2 -s 640x480 -r 10 -i /dev/video0 %03d.jpeg')
+    if os.path.exists(fn):
+        res = fn
+    return res
 
 token = config.bot_token
 allow_users = config.allow_users.split(',')
 bot = telebot.TeleBot(token)
-bot.config['api_key'] = token
+#bot.config['api_key'] = token
 
 print 'init gpio'
 gpio.init()
@@ -45,9 +54,15 @@ try:
         if stop - start > 0.2:
             print 'bell'
             n = False
+            fn = take_picture()
             for b in allow_users:
                 bot.send_message(b, u'В дверь звонят!')
+                if fn:
+                    with open(fn, 'rb') as f:
+                        bot.send_photo(b, f)
             sleep(1)
+            if fn:
+                os.remove(fn)
         sleep(0.01)
 except KeyboardInterrupt:
     gpio.output(pin_out, gpio.LOW)
